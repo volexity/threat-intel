ee8bd89d5255fabbea2433e461a71f72782cc4ba
Tom Lancaster <tlancaster@volexity.com>
2023-09-05 14:48:38 +0100

    moving one-extract project into its own repository

--- tools/one-extract/cli_proxy.py
+++
@@ -1,8 +0,0 @@
-# OneNoteExtractor
-# Copyright (C) 2023 Volexity, Inc.
-
-"""Shortcut to test cli.py without installing the package."""
-
-from onenoteextractor.cli import run
-
-run()
--- tools/one-extract/onenoteextractor/__init__.py
+++
@@ -1,1 +0,0 @@
-from .one import OneNoteExtractor  # noqa:F401
